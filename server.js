a2c8c2fc22e049f7dc97f5ba4463b78d8433dca2
Alex Lang <langalex4@gmail.com>
2015-02-26 09:41:56 -0600

    finished tutorial

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('Hello World');
-});
+require('./router/main')(app); // add main.js and pass app to it
+
+app.set('views', __dirname + '/views'); // tell server where html files live
+
+app.set('view engine', 'ejs'); // set view engine to ejs, which is responsible for rendering html
+
+app.engine('html', require('ejs').renderFile); // tells server to render html files via ejs
 
 var server = app.listen(3000, function() {
 	console.log("Started server on port 3000");
